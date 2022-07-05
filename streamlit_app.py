33bf80236aabe825d9ce21865c9a34980955cf1c
Poux-Be <louis.poux@bearingpoint.com>
2022-07-05 18:16:08 +0200

    snowflake connector test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # App file
 
 # ----- Imports -----
-from ast import Add
 import os
 import requests
 import streamlit
@@ -32,9 +31,9 @@ def get_fruityvice_data(this_fruit_choice):
     return(fruityvice_normalized)
 
 # Fetch the fruit list from Snowflake
-def get_fruit_load_list():
+def get_table(table_name):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
+        my_cur.execute("select * from "+table_name)
         return (my_cur.fetchall())
 
 # Add a row into Snowflake
@@ -48,6 +47,14 @@ streamlit.title("D&A Challenge - 2")
 
 # Simple menu
 streamlit.header('Data received')
+# Query snowflake
+streamlit.header("The fruit list contains:")
+# Add a button to query the fruit list
+if streamlit.button("Get the intial data"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_table("sales")
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
 
 
 # Don't run anything past here while troubleshooting
