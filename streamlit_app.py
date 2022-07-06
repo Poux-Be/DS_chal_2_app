5ab99b845eba6ffaea798b9913bb5c4eff6a9140
Poux-Be <louis.poux@bearingpoint.com>
2022-07-06 16:15:28 +0200

    select count 10

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ st.text('')
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 with my_cnx.cursor() as my_cur:
-    my_cur.execute("select transaction_date, count(*) over (partition by transaction_date) as daily_sales_count from sales where (transaction_date <= '"+d2.strftime('%Y-%m-%d')+"' and transaction_date >= '"+d1.strftime('%Y-%m-%d')+"' and local_type='Appartement') order by transaction_date asc")
+    my_cur.execute("select transaction_date, count(*) over (partition by transaction_date) as daily_sales_count from sales where (transaction_date <= '"+d2.strftime('%Y-%m-%d')+"' and transaction_date >= '"+d1.strftime('%Y-%m-%d')+"' and local_type='Appartement') group by transaction_date order by transaction_date asc")
     header = [x[0] for x in my_cur.description]
     my_query_results = pd.DataFrame(my_cur.fetchall(), columns = header)
 my_cnx.close()
