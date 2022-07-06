63122af6d31921464156d9a455cf2c8cff8cd51c
Poux-Be <louis.poux@bearingpoint.com>
2022-07-06 16:33:46 +0200

    chart 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ st.subheader(''+str(sum((my_query_results[my_query_results['LOCAL_TYPE']=='Appar
 my_cnx.close()
 
 #dataframe formatting to have a beautiful chart
-chart_df = pd.pivot_table(my_query_results, values=my_query_results['DAILY_SALES_COUNT'],index=my_query_results['TRANSACTION_DATE'], columns=my_query_results['LOCAL_TYPE'])
+chart_df = pd.pivot_table(my_query_results, values=['DAILY_SALES_COUNT'],index=['TRANSACTION_DATE'], columns=['LOCAL_TYPE'])
 st.dataframe(chart_df)
 
 
