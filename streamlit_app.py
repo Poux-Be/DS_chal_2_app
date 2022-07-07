82c8d4806526acf6316c646ebe9292a0670e4b99
Poux-Be <louis.poux@bearingpoint.com>
2022-07-07 13:34:34 +0200

    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,7 +131,7 @@ default = my_query_results if len(my_query_results) <= 10 else 10
 top = st.slider('How many departments do you want to see?', 0, len(my_query_results), default)
 
 #answer the exercise question
-st.bar_chart(my_query_results[:top])
+st.bar_chart(my_query_results[:10])
 
 
 
