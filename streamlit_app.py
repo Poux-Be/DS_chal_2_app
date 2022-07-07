1d0efbb9fb212196b62152b5618d3e9a2e632519
Poux-Be <louis.poux@bearingpoint.com>
2022-07-07 10:38:00 +0200

    test left merge

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,7 +136,7 @@ st.text(type(df_departement['INSEE_CODE'].to_list()[0]))
 st.text(type(my_query_results['DEPT_CODE'].to_list()[0]))
 st.text(temp_list)
 
-my_query_results = my_query_results.merge(df_departement, how='outer', left_on = 'DEPT_CODE', right_on = 'INSEE_CODE')
+my_query_results = my_query_results.merge(df_departement, how='left', left_on = 'DEPT_CODE', right_on = 'INSEE_CODE')
 
 #answer the exercise question
 st.dataframe(my_query_results)
