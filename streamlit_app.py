9e59c9f8ce44b8b9a8a5a0795c54e8130e56a197
Poux-Be <louis.poux@bearingpoint.com>
2022-07-07 14:59:29 +0200

    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,12 +145,12 @@ st.header('Fourth query: Average squarred meter price for a department 🏡/🏢
 
 # Dept code input
 region_list = execute_sf_query_table("select distinct new_region from dept_info")['NEW_REGION'].to_list()
-selected_region = st.selectbox("Veuillez choisir la région dont vous voulez foir le prix moyen", region_list)
+selected_region = st.selectbox("Veuillez choisir la région dont vous voulez foir le prix moyen", region_list).replace("'","\'")
 st.text(" Your selection: "+selected_region)
 
 if st.button('Display the average sqm price'):
     # Snowflake query
-    dept_list = execute_sf_query_table("select insee_code from dept_info where new_region ='"+str(selected_region).replace("'","\'")+"'")['INSEE_CODE'].to_list()
+    dept_list = execute_sf_query_table("select insee_code from dept_info where new_region ='"+selected_region+"'")['INSEE_CODE'].to_list()
     st.text(str(dept_list).replace('[',''))
     my_query_results_4 = execute_sf_query_table("select local_type, avg(transaction_value/carrez_surface) as avg_sqm_price from sales_view where dept_code is in ("+str(dept_list).replace('[','')+") group by local_type order by avg_sqm_price desc")
 
